029489e2dbd6c087d9df4be6c560522d0b6f8162
loki <loki@devone.attlocal.net>
2024-05-13 14:44:50 -0500

    add

--- GreatestCommonDivisorofStrings1071.py
+++ GreatestCommonDivisorofStrings1071.py
@@ -2,4 +2,9 @@
 
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-
+        startingPoints = []
+        leadingChar = str2[0]
+        for x, char in enumerate(str1):
+            if char == leadingChar:
+                startingPoints.append(x)
+        print(startingPoints)
