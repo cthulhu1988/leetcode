b1894c05eb73f4dccb8855d99aba0a638449c114
Isaac <isaac@callison.tech>
2024-05-21 22:31:17 -0500

    add

--- GreatestCommonDivisorofStrings1071.py
+++ GreatestCommonDivisorofStrings1071.py
@@ -9,41 +9,47 @@ class Solution:
                 startingPoints.append(x)
         snippets = self.compareStrings(str1, str2, startingPoints)
         snip = "".join(snippets)
-        print(snip)
+        #print(snip)
     
 
     def compareStrings(self, str1:str, str2:str, startingPoints) -> str:
-        list = set()
-        for point in startingPoints:
-            pace1 = 0
-            pace2 = point
-            while str1[pace2] == str2[pace1] and pace1 <= len(str2):
-                list.add(str2[pace1])
-                #print(str1[pace2], str2[pace1])
-                pace1 += 1
-                pace2 += 1
-                if pace1 >= len(str2): 
+        mylist = set()
+        x=[]
+        x[:0] = str1
+        #print("list",x)
+        for char in str2:
+            if char not in x:
+                print(char)
+                mylist = ""
+                return mylist
+            else:
+                for point in startingPoints:
                     pace1 = 0
-                    break
-                else: 
-                    pace1
+                    pace2 = point
+                    while str1[pace2] == str2[pace1] and pace1 <= len(str2):
+                        mylist.add(str2[pace1])
+                        pace1 += 1
+                        pace2 += 1
+                        if pace1 >= len(str2): 
+                            pace1 = 0
+                            break
+                        else: 
+                            pace1
                 
-                if pace2 >= len(str1): 
-                    break
-            
-                #print(str1[pace2], str2[pace1])
-        return sorted(list)
+                        if pace2 >= len(str1): 
+                            break
+        return sorted(mylist)
 
 sol = Solution()
 
 #str1 = "ABCABC"
 #str2 = "ABC"
 
-str1 = "ABABAB"
-str2 = "ABAB"
+#str1 = "ABABAB"
+#str2 = "ABAB"
 
-#str1 = "LEET"
-#str2 = "CODE"
+str1 = "LEET"
+str2 = "CODE"
 
 sol.gcdOfStrings(str1, str2)
 
